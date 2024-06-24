51e08bfbc4339becb288c3d13247548ddf38d1c8
toneblock <55140928+Toneblock@users.noreply.github.com>
2024-06-24 17:40:41 +0400

    initial upload

---
+++ README.md
@@ -0,0 +1,10 @@
+# NotJustYet
+(Steamodded 1.0) Prevents scoring from ending the round early
+
+# Installation
+
+Install [Lovely](https://github.com/ethangreen-dev/lovely-injector)
+Install [Steamodded](https://github.com/Steamopollys/Steamodded) (must be **1.0.0 or higher**)
+
+
+Download the newest release, extract the .zip, and place it into your Mods folder.
---
+++ main.lua
@@ -0,0 +1,39 @@
+--- STEAMODDED HEADER
+--- MOD_NAME: NotJustYet
+--- MOD_ID: NotJustYet
+--- PREFIX: njy
+--- MOD_AUTHOR: [toneblock]
+--- MOD_DESCRIPTION: Prevents scoring from ending the round early
+--- PRIORITY: 0
+--- DISPLAY_NAME: NotJustYet
+--- BADGE_COLOUR: ffffff
+
+----------------------------------------------
+------------MOD CODE -------------------------
+
+G.FUNCS.njy_can_endround = function(e)
+	if G.GAME.chips - G.GAME.blind.chips >= 0 then
+		e.config.colour = G.C.GREEN
+		e.config.button = 'njy_endround'
+	else
+		e.config.colour = G.C.UI.BACKGROUND_INACTIVE
+		e.config.button = nil
+	end
+end
+
+G.FUNCS.njy_endround = function(e)
+	stop_use()
+	G.STATE = G.STATES.NEW_ROUND
+	end_round()
+end
+
+SMODS.Atlas({
+    key = "modicon",
+    path = "njy_icon.png",
+    px = 34,
+    py = 34
+}):register()
+
+
+----------------------------------------------
+------------MOD CODE END----------------------
